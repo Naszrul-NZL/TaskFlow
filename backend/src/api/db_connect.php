e67abbe20a83d723fab89ae091bb2c983c6d5bc1
Naszrul-NZL <muhdnaszrul17@gmail.com>
2026-06-25 17:56:53 +0800

    Add CORS and OPTIONS preflight handling

--- backend/src/api/db_connect.php
+++ backend/src/api/db_connect.php
@@ -1,4 +1,12 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 function getDB() {
     $dbPath = 'C:/xampp/htdocs/TaskFlow/db/database.sqlite';
     
--- backend/src/api/index.php
+++ backend/src/api/index.php
@@ -1,5 +1,14 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 
 try {
     $dbPath = 'C:/xampp/htdocs/TaskFlow/db/database.sqlite';
